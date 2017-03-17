736d7eef3b7dc79214130abc438ecfdc86ae4067
jonathanvdc <jonathan.vdc@outlook.com>
2017-03-17 18:29:07 +0100

    Add LEB128 reading logic to the binary Wasm reader

--- libwasm/Binary/BinaryWasmReader.cs
+++ libwasm/Binary/BinaryWasmReader.cs
@@ -20,7 +20,7 @@ namespace Wasm.Binary
         /// <summary>
         /// The binary reader for a WebAssembly file.
         /// </summary>
-        private BinaryReader Reader;
+        public BinaryReader Reader { get; private set; }
 
         /// <summary>
         /// Parses a version header.
@@ -29,6 +29,113 @@ namespace Wasm.Binary
         public VersionHeader ParseVersionHeader()
         {
             return new VersionHeader(Reader.ReadUInt32(), Reader.ReadUInt32());
+        }
+
+        /// <summary>
+        /// Parses an unsigned LEB128 variable-length integer, limited to 64 bits.
+        /// </summary>
+        /// <returns>The parsed unsigned 64-bit integer.</returns>
+        public ulong ReadVarUInt64()
+        {
+            // C# translation of code borrowed from Wikipedia article:
+            // https://en.wikipedia.org/wiki/LEB128
+            ulong result = 0;
+            int shift = 0;
+            while (true) 
+            {
+                byte b = Reader.ReadByte();
+                result |= ((ulong)(b & 0x7F) << shift);
+                if ((b & 0x80) == 0)
+                    break;
+                shift += 7;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Parses an unsigned LEB128 variable-length integer, limited to one bit.
+        /// </summary>
+        /// <returns>The parsed unsigned 1-bit integer, as a Boolean.</returns>
+        public bool ReadVarUInt1()
+        {
+            // Negate the integer twice to turn it into a Boolean.
+            return !(ReadVarUInt64() == 0);
+        }
+
+        /// <summary>
+        /// Parses an unsigned LEB128 variable-length integer, limited to 7 bits.
+        /// </summary>
+        /// <returns>The parsed unsigned 7-bit integer.</returns>
+        public byte ReadVarUInt7()
+        {
+            return (byte)ReadVarUInt64();
+        }
+
+        /// <summary>
+        /// Parses an unsigned LEB128 variable-length integer, limited to 32 bits.
+        /// </summary>
+        /// <returns>The parsed unsigned 32-bit integer.</returns>
+        public uint ReadVarUInt32()
+        {
+            return (uint)ReadVarUInt64();
+        }
+
+        /// <summary>
+        /// Parses a signed LEB128 variable-length integer of variable size,
+        /// which is at most 64 bits.
+        /// </summary>
+        /// <param name="Size">The size of the variable-length integer, in bits.</param>
+        /// <returns>The parsed signed 64-bit integer.</returns>
+        public long ReadVarInt(int Size)
+        {
+            // C# translation of code borrowed from Wikipedia article:
+            // https://en.wikipedia.org/wiki/LEB128
+
+            long result = 0;
+            int shift = 0;
+            byte b;
+            do
+            {
+                b = Reader.ReadByte();
+                result |= ((long)(b & 0x7F) << shift);
+                shift += 7;
+            } while ((b & 0x80) != 0);
+
+            // Sign bit of byte is second high order bit. (0x40)
+            if ((shift < Size) && ((b & 0x40) == 1))
+            {
+                // Sign extend.
+                result |= -(1L << shift);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Parses a signed LEB128 variable-length integer, limited to 7 bits.
+        /// </summary>
+        /// <returns>The parsed signed 7-bit integer.</returns>
+        public sbyte ReadVarInt7()
+        {
+            return (sbyte)ReadVarInt(7);
+        }
+
+        /// <summary>
+        /// Parses a signed LEB128 variable-length integer, limited to 32 bits.
+        /// </summary>
+        /// <returns>The parsed signed 32-bit integer.</returns>
+        public int ReadVarInt32()
+        {
+            return (int)ReadVarInt(32);
+        }
+
+        /// <summary>
+        /// Parses a signed LEB128 variable-length integer, limited to 64 bits.
+        /// </summary>
+        /// <returns>The parsed signed 64-bit integer.</returns>
+        public long ReadVarInt64()
+        {
+            return ReadVarInt(64);
         }
     }
 }

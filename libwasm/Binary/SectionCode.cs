3207a66f28292507f366a80ba8b03c89e9a9145c
jonathanvdc <jonathan.vdc@outlook.com>
2017-03-18 19:25:06 +0100

    Wrap some data structures in the Wasm.Binary namespace

--- libwasm/Binary/SectionCode.cs
+++ libwasm/Binary/SectionCode.cs
@@ -1,65 +1,68 @@
-/// <summary>
-/// Enumerates possible section codes.
-/// </summary>
-public enum SectionCode
+namespace Wasm.Binary
 {
     /// <summary>
-    /// The section code for custom sections.
+    /// /// Enumerates possible section codes.
     /// </summary>
-    Custom = 0,
+    public enum SectionCode
+    {
+        /// <summary>
+        /// The section code for custom sections.
+        /// </summary>
+        Custom = 0,
 
-    /// <summary>
-    /// The section code for function signature declarations.
-    /// </summary>
-    Type = 1,
+        /// <summary>
+        /// The section code for function signature declarations.
+        /// </summary>
+        Type = 1,
 
-    /// <summary>
-    /// The section code for import declarations.
-    /// </summary>
-    Import = 2,
+        /// <summary>
+        /// The section code for import declarations.
+        /// </summary>
+        Import = 2,
 
-    /// <summary>
-    /// The section code for function declarations.
-    /// </summary>
-    Function = 3,
+        /// <summary>
+        /// The section code for function declarations.
+        /// </summary>
+        Function = 3,
 
-    /// <summary>
-    /// The section code for tables, e.g., the indirect function table.
-    /// </summary>
-    Table = 4,
+        /// <summary>
+        /// The section code for tables, e.g., the indirect function table.
+        /// </summary>
+        Table = 4,
 
-    /// <summary>
-    /// The section code for memory attributes.
-    /// </summary>
-    Memory = 5,
+        /// <summary>
+        /// The section code for memory attributes.
+        /// </summary>
+        Memory = 5,
 
-    /// <summary>
-    /// The section code for global declarations.
-    /// </summary>
-    Global = 6,
+        /// <summary>
+        /// The section code for global declarations.
+        /// </summary>
+        Global = 6,
 
-    /// <summary>
-    /// The section code for exports.
-    /// </summary>
-    Export = 7,
+        /// <summary>
+        /// The section code for exports.
+        /// </summary>
+        Export = 7,
 
-    /// <summary>
-    /// The section code for the start function declarations.
-    /// </summary>
-    Start = 8,
+        /// <summary>
+        /// The section code for the start function declarations.
+        /// </summary>
+        Start = 8,
 
-    /// <summary>
-    /// The section code for an elements section.
-    /// </summary>
-    Element = 9,
+        /// <summary>
+        /// The section code for an elements section.
+        /// </summary>
+        Element = 9,
 
-    /// <summary>
-    /// The section code for function bodies.
-    /// </summary>
-    Code = 10,
+        /// <summary>
+        /// The section code for function bodies.
+        /// </summary>
+        Code = 10,
 
-    /// <summary>
-    /// The section code for data segments.
-    /// </summary>
-    Data = 11
+        /// <summary>
+        /// The section code for data segments.
+        /// </summary>
+        Data = 11
+    }
 }
--- libwasm/Binary/SectionHeader.cs
+++ libwasm/Binary/SectionHeader.cs
@@ -1,56 +1,59 @@
-/// <summary>
-/// Represents a section's header.
-/// </summary>
-public struct SectionHeader
+namespace Wasm.Binary
 {
     /// <summary>
-    /// Creates a section header for a non-custom section with the given section
-    /// code and payload length.
+    /// /// Represents a section's header.
     /// </summary>
-    /// <param name="Code">The section code</param>
-    /// <param name="PayloadLength">The length of the payload.</param>
-    public SectionHeader(SectionCode Code, uint PayloadLength)
+    public struct SectionHeader
     {
-        this.Code = Code;
-        this.PayloadLength = PayloadLength;
-        this.SectionName = default(ByteString);
+        /// <summary>
+        /// Creates a section header for a non-custom section with the given section
+        /// code and payload length.
+        /// </summary>
+        /// <param name="Code">The section code</param>
+        /// <param name="PayloadLength">The length of the payload.</param>
+        public SectionHeader(SectionCode Code, uint PayloadLength)
+        {
+            this.Code = Code;
+            this.PayloadLength = PayloadLength;
+            this.SectionName = default(ByteString);
+        }
+
+        /// <summary>
+        /// Creates a section header for a custom section with the given name and
+        /// payload length.
+        /// </summary>
+        /// <param name="SectionName">The name of the custom section</param>
+        /// <param name="PayloadLength">The length of the payload.</param>
+        public SectionHeader(ByteString SectionName, uint PayloadLength)
+        {
+            this.Code = SectionCode.Custom;
+            this.PayloadLength = PayloadLength;
+            this.SectionName = SectionName;
+        }
+
+        /// <summary>
+        /// Gets the section's code.
+        /// </summary>
+        /// <returns>The section code.</returns>
+        public SectionCode Code { get; private set; }
+
+        /// <summary>
+        /// Gets a Boolean value that tells if
+        /// </summary>
+        public bool IsCustom => Code == SectionCode.Custom;
+
+        /// <summary>
+        /// Gets the length of the payload, in bytes.
+        /// </summary>
+        /// <returns>The length of the payload, in bytes.</returns>
+        public uint PayloadLength { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the section, as a byte string. This applies only to
+        /// custom sections.
+        /// </summary>
+        /// <returns>The name of the section if is this a custom section;
+        /// otherwise, the null byte string.</returns>
+        public ByteString SectionName { get; private set; }
     }
-
-    /// <summary>
-    /// Creates a section header for a custom section with the given name and
-    /// payload length.
-    /// </summary>
-    /// <param name="SectionName">The name of the custom section</param>
-    /// <param name="PayloadLength">The length of the payload.</param>
-    public SectionHeader(ByteString SectionName, uint PayloadLength)
-    {
-        this.Code = SectionCode.Custom;
-        this.PayloadLength = PayloadLength;
-        this.SectionName = SectionName;
-    }
-
-    /// <summary>
-    /// Gets the section's code.
-    /// </summary>
-    /// <returns>The section code.</returns>
-    public SectionCode Code { get; private set; }
-
-    /// <summary>
-    /// Gets a Boolean value that tells if
-    /// </summary>
-    public bool IsCustom => Code == SectionCode.Custom;
-
-    /// <summary>
-    /// Gets the length of the payload, in bytes.
-    /// </summary>
-    /// <returns>The length of the payload, in bytes.</returns>
-    public uint PayloadLength { get; private set; }
-
-    /// <summary>
-    /// Gets the name of the section, as a byte string. This applies only to
-    /// custom sections.
-    /// </summary>
-    /// <returns>The name of the section if is this a custom section;
-    /// otherwise, the null byte string.</returns>
-    public ByteString SectionName { get; private set; }
 }

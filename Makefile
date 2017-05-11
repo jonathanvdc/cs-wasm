265c26b9e22f2e81e64ece849df4906b2f7dfb93
jonathanvdc <jonathan.vdc@outlook.com>
2017-05-11 18:12:02 +0200

    Create wasm-cat, which concatenates WebAssembly file sections

--- Makefile
+++ Makefile
@@ -1,26 +1,31 @@
 exe:
 	make -C libwasm flo
 	make -C nullary-opcode-generator exe
+	make -C wasm-cat exe
 	make -C wasm-dump exe
 
 all:
 	make -C libwasm all
 	make -C nullary-opcode-generator all
+	make -C wasm-cat all
 	make -C wasm-dump all
 
 dll:
 	make -C libwasm dll
 	make -C nullary-opcode-generator exe
+	make -C wasm-cat exe
 	make -C wasm-dump exe
 
 flo:
 	make -C libwasm flo
 	make -C nullary-opcode-generator flo
+	make -C wasm-cat flo
 	make -C wasm-dump flo
 
 clean:
 	make -C libwasm clean
 	make -C nullary-opcode-generator clean
+	make -C wasm-cat clean
 	make -C wasm-dump clean
 
 test: exe
---
+++ wasm-cat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Wasm.Binary;
+
+namespace Wasm.Cat
+{
+    // wasm-cat concatenates the contents of WebAssembly files by merging their tables.
+
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("usage: wasm-cat file.wasm...");
+                return 1;
+            }
+
+            var file = new WasmFile();
+            foreach (var path in args)
+            {
+                using (var fileStream = File.OpenRead(path))
+                {
+                    using (var reader = new BinaryReader(fileStream))
+                    {
+                        // Create a WebAssembly reader, read the file and append its
+                        // sections to the resulting file.
+                        var wasmReader = new BinaryWasmReader(reader);
+                        file.Sections.AddRange(wasmReader.ReadFile().Sections);
+                    }
+                }
+            }
+
+            // Now write the file to standard output.
+            using (var outputStream = Console.OpenStandardOutput())
+            {
+                using (var writer = new BinaryWriter(outputStream))
+                {
+                    var wasmWriter = new BinaryWasmWriter(writer);
+                    wasmWriter.WriteFile(file);
+                }
+            }
+            return 0;
+        }
+    }
+}

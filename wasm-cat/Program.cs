58e9eb3222a9eb47129e76ea2fc8e33d63698e5e
jonathanvdc <jonathan.vdc@outlook.com>
2017-05-12 16:28:07 +0200

    Define an -o option for wasm-cat

--- wasm-cat/Program.cs
+++ wasm-cat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Wasm.Binary;
 
@@ -6,18 +7,55 @@ namespace Wasm.Cat
 {
     // wasm-cat takes WebAssembly files as input and concatenates their sections.
 
+    public struct CatArgs
+    {
+        public string Output { get; set; }
+
+        public IEnumerable<string> Inputs { get; set; }
+
+        public static bool TryParse(string[] Args, out CatArgs Result)
+        {
+            Result = default(CatArgs);
+            if (Args.Length == 0)
+            {
+                return false;
+            }
+
+            var inputs = new List<string>();
+            for (int i = 0; i < Args.Length; i++)
+            {
+                if (Args[i] == "-o")
+                {
+                    if (i == Args.Length - 1)
+                    {
+                        return false;
+                    }
+                    i++;
+                    Result.Output = Args[i];
+                }
+                else
+                {
+                    inputs.Add(Args[i]);
+                }
+            }
+            Result.Inputs = inputs;
+            return true;
+        }
+    }
+
     public static class Program
     {
         public static int Main(string[] args)
         {
-            if (args.Length < 1)
+            CatArgs parsedArgs;
+            if (!CatArgs.TryParse(args, out parsedArgs))
             {
-                Console.Error.WriteLine("usage: wasm-cat file.wasm...");
+                Console.Error.WriteLine("usage: wasm-cat file.wasm... [-o output.wasm]");
                 return 1;
             }
 
             var file = new WasmFile();
-            foreach (var path in args)
+            foreach (var path in parsedArgs.Inputs)
             {
                 using (var fileStream = File.OpenRead(path))
                 {
@@ -40,7 +78,9 @@ namespace Wasm.Cat
             }
 
             // Now write the file to standard output.
-            using (var outputStream = Console.OpenStandardOutput())
+            using (var outputStream = string.IsNullOrEmpty(parsedArgs.Output)
+                ? Console.OpenStandardOutput()
+                : File.OpenWrite(parsedArgs.Output))
             {
                 using (var writer = new BinaryWriter(outputStream))
                 {

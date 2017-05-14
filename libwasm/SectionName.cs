13ab4d9e9669d55725f5bb4ba33f0304cd3ebf0d
jonathanvdc <jonathan.vdc@outlook.com>
2017-05-14 15:11:45 +0200

    Implement equality for SectionName.cs

--- libwasm/SectionName.cs
+++ libwasm/SectionName.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace Wasm
 {
     /// <summary>
     /// /// Represents a section's header.
     /// </summary>
-    public struct SectionName
+    public struct SectionName : IEquatable<SectionName>
     {
         /// <summary>
         /// Creates a section name for a non-custom section with the given section code.
@@ -43,6 +45,65 @@ namespace Wasm
         /// <returns>The name of the section if is this a custom section; otherwise, null.</returns>
         public string CustomName { get; private set; }
 
+        /// <summary>
+        /// Checks if this section name is equal to the given section name.
+        /// </summary>
+        /// <param name="Other">The other section name.</param>
+        /// <returns><c>true</c> if this section name is equal to the given section name; otherwise, <c>false</c>.</returns>
+        public bool Equals(SectionName Other)
+        {
+            if (IsCustom)
+            {
+                return Other.IsCustom && CustomName == Other.CustomName;
+            }
+            else
+            {
+                return Code == Other.Code;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            if (IsCustom)
+            {
+                return CustomName.GetHashCode();
+            }
+            else
+            {
+                return (int)Code;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object Other)
+        {
+            return Other is SectionName && Equals((SectionName)Other);
+        }
+
+        /// <summary>
+        /// Checks if the given section names are the same.
+        /// </summary>
+        /// <param name="First">The first section name.</param>
+        /// <param name="Second">The second section name.</param>
+        /// <returns><c>true</c> if the given section names are the same; otherwise, <c>false</c>.</returns>
+        public static bool operator==(SectionName First, SectionName Second)
+        {
+            return First.Equals(Second);
+        }
+
+        /// <summary>
+        /// Checks if the given section names not are the same.
+        /// </summary>
+        /// <param name="First">The first section name.</param>
+        /// <param name="Second">The second section name.</param>
+        /// <returns><c>true</c> if the given section names are not the same; otherwise, <c>false</c>.</returns>
+        public static bool operator!=(SectionName First, SectionName Second)
+        {
+            return !First.Equals(Second);
+        }
+
+        /// <inheritdoc/>
         public override string ToString()
         {
             if (IsCustom)

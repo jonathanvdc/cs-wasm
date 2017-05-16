976d2a2a68553962253e9de39d27bca7f8942152
jonathanvdc <jonathan.vdc@outlook.com>
2017-05-16 12:21:34 +0200

    Support accessing linear memory

--- libwasm-interpret/LinearMemory.cs
+++ libwasm-interpret/LinearMemory.cs
@@ -60,5 +60,164 @@ namespace Wasm.Interpret
         {
             return GrowToSize(Size + NumberOfPages);
         }
+
+        /// <summary>
+        /// Accesses linear memory as a sequence of 8-bit signed integers.
+        /// </summary>
+        /// <returns>A view of this memory.</returns>
+        public LinearMemoryAsInt8 Int8
+        {
+            get { return new LinearMemoryAsInt8(memory); }
+        }
+
+        /// <summary>
+        /// Accesses linear memory as a sequence of 16-bit signed integers.
+        /// </summary>
+        /// <returns>A view of this memory.</returns>
+        public LinearMemoryAsInt16 Int16
+        {
+            get { return new LinearMemoryAsInt16(memory); }
+        }
+
+        /// <summary>
+        /// Accesses linear memory as a sequence of 32-bit signed integers.
+        /// </summary>
+        /// <returns>A view of this memory.</returns>
+        public LinearMemoryAsInt32 Int32
+        {
+            get { return new LinearMemoryAsInt32(memory); }
+        }
+
+        /// <summary>
+        /// Accesses linear memory as a sequence of 64-bit signed integers.
+        /// </summary>
+        /// <returns>A view of this memory.</returns>
+        public LinearMemoryAsInt64 Int64
+        {
+            get { return new LinearMemoryAsInt64(memory); }
+        }
+    }
+
+    /// <summary>
+    /// Accesses linear memory as a sequence of 8-bit signed integers.
+    /// </summary>
+    public struct LinearMemoryAsInt8
+    {
+        public LinearMemoryAsInt8(List<byte> Memory)
+        {
+            this.mem = Memory;
+        }
+
+        private List<byte> mem;
+
+        public sbyte this[uint Offset]
+        {
+            get
+            {
+                return (sbyte)mem[(int)Offset];
+            }
+            set
+            {
+                mem[(int)Offset] = (byte)value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Accesses linear memory as a sequence of 16-bit signed integers.
+    /// </summary>
+    public struct LinearMemoryAsInt16
+    {
+        public LinearMemoryAsInt16(List<byte> Memory)
+        {
+            this.mem = Memory;
+        }
+
+        private List<byte> mem;
+
+        public short this[uint Offset]
+        {
+            get
+            {
+                return (short)(
+                    (uint)mem[(int)Offset + 1] << 8 |
+                    (uint)mem[(int)Offset]);
+            }
+            set
+            {
+                mem[(int)Offset + 1] = (byte)(value >> 8);
+                mem[(int)Offset] = (byte)value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Accesses linear memory as a sequence of 32-bit signed integers.
+    /// </summary>
+    public struct LinearMemoryAsInt32
+    {
+        public LinearMemoryAsInt32(List<byte> Memory)
+        {
+            this.mem = Memory;
+        }
+
+        private List<byte> mem;
+
+        public int this[uint Offset]
+        {
+            get
+            {
+                return (int)mem[(int)Offset + 3] << 24
+                    | (int)mem[(int)Offset + 2] << 16
+                    | (int)mem[(int)Offset + 1] << 8
+                    | (int)mem[(int)Offset];
+            }
+            set
+            {
+                mem[(int)Offset + 3] = (byte)(value >> 24);
+                mem[(int)Offset + 2] = (byte)(value >> 16);
+                mem[(int)Offset + 1] = (byte)(value >> 8);
+                mem[(int)Offset] = (byte)value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Accesses linear memory as a sequence of 32-bit signed integers.
+    /// </summary>
+    public struct LinearMemoryAsInt64
+    {
+        public LinearMemoryAsInt64(List<byte> Memory)
+        {
+            this.mem = Memory;
+        }
+
+        private List<byte> mem;
+
+        public long this[uint Offset]
+        {
+            get
+            {
+                return (long)mem[(int)Offset + 7] << 56
+                    | (long)mem[(int)Offset + 6] << 48
+                    | (long)mem[(int)Offset + 5] << 40
+                    | (long)mem[(int)Offset + 4] << 32
+                    | (long)mem[(int)Offset + 3] << 24
+                    | (long)mem[(int)Offset + 2] << 16
+                    | (long)mem[(int)Offset + 1] << 8
+                    | (long)mem[(int)Offset];
+            }
+            set
+            {
+                mem[(int)Offset + 7] = (byte)(value >> 56);
+                mem[(int)Offset + 6] = (byte)(value >> 48);
+                mem[(int)Offset + 5] = (byte)(value >> 40);
+                mem[(int)Offset + 4] = (byte)(value >> 32);
+                mem[(int)Offset + 3] = (byte)(value >> 24);
+                mem[(int)Offset + 2] = (byte)(value >> 16);
+                mem[(int)Offset + 1] = (byte)(value >> 8);
+                mem[(int)Offset] = (byte)value;
+            }
+        }
     }
 }

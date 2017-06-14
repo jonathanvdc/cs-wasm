6c25e9fd28a3971d091a76849a37f8eddac3207b
jonathanvdc <jonathan.vdc@outlook.com>
2017-06-14 19:32:51 +0200

    Define FunctionBodyOptimizations.ExpandLocalEntries

--- libwasm-optimize/FunctionBodyOptimizations.cs
+++ libwasm-optimize/FunctionBodyOptimizations.cs
@@ -50,6 +50,32 @@ namespace Wasm.Optimize
             Body.Locals.Clear();
             Body.Locals.AddRange(newLocals);
         }
+
+        /// <summary>
+        /// Modifies the function body's local declarations such that every entry
+        /// declares exactly one local. Empty local entries are deleted and local
+        /// entries that declare n locals are replaced by n local entries that
+        /// declare one local.
+        /// </summary>
+        /// <param name="Body">The function body to update.</param>
+        public static void ExpandLocalEntries(this FunctionBody Body)
+        {
+            // Create an equivalent list of local entries in which all local
+            // entries declare exactly one local.
+            var newLocals = new List<LocalEntry>();
+            for (int i = 0; i < Body.Locals.Count; i++)
+            {
+                var currentEntry = Body.Locals[i];
+                for (uint j = 0; j < currentEntry.LocalCount; j++)
+                {
+                    newLocals.Add(new LocalEntry(currentEntry.LocalType, 1));
+                }
+            }
+
+            // Clear the old local list and replace its contents with the new entries.
+            Body.Locals.Clear();
+            Body.Locals.AddRange(newLocals);
+        }
     }
 }
 

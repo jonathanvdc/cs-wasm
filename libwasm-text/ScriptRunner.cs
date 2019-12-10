ae973884bb4e9a99eae1fcc46a6a20c4c5767812
jonathanvdc <jonathan.vdc@outlook.com>
2019-12-10 22:59:36 +0100

    Make test runner tally test results

--- libwasm-text/ScriptRunner.cs
+++ libwasm-text/ScriptRunner.cs
@@ -45,24 +45,110 @@ namespace Wasm.Text
         private Dictionary<string, ModuleInstance> moduleInstancesByName;
 
         /// <summary>
+        /// A data structure that tallies the number of tests that were run.
+        /// </summary>
+        public struct TestStatistics
+        {
+            /// <summary>
+            /// Initializes an instance of the <see cref="TestStatistics"/> type.
+            /// </summary>
+            /// <param name="successfulCommandCount">
+            /// The number of commands that were executed successfully.
+            /// </param>
+            /// <param name="failedCommandCount">
+            /// The number of commands that were executed unsuccessfully.
+            /// </param>
+            /// <param name="unknownCommandCount">
+            /// The number of command expressions that were skipped because they were not recognized as a known command.
+            /// </param>
+            public TestStatistics(int successfulCommandCount, int failedCommandCount, int unknownCommandCount)
+            {
+                this.SuccessfulCommandCount = successfulCommandCount;
+                this.FailedCommandCount = failedCommandCount;
+                this.UnknownCommandCount = unknownCommandCount;
+            }
+
+            /// <summary>
+            /// Gets the number of commands that were recognized and successfully executed.
+            /// </summary>
+            /// <value>The number of successfully executed commands.</value>
+            public int SuccessfulCommandCount { get; private set; }
+
+            /// <summary>
+            /// Gets the number of commands that were recognized and executed, but then errored in some way.
+            /// </summary>
+            /// <value>The number of commands that failed.</value>
+            public int FailedCommandCount { get; private set; }
+
+            /// <summary>
+            /// Gets the number of command expressions that could not be recognized as a
+            /// known command and were hence skipped.
+            /// </summary>
+            /// <value>The number of unrecognized commands.</value>
+            public int UnknownCommandCount { get; private set; }
+
+            /// <summary>
+            /// Gets the total number of commands expressions that were encountered.
+            /// </summary>
+            public int TotalCommandCount => SuccessfulCommandCount + FailedCommandCount + UnknownCommandCount;
+
+            /// <summary>
+            /// Computes the elementwise sum of two test statistics.
+            /// </summary>
+            /// <param name="left">A first set of test statistics.</param>
+            /// <param name="right">A second set of test statistics.</param>
+            /// <returns>The elementwise sum of <paramref name="left"/> and <paramref name="right"/>.</returns>
+            public static TestStatistics operator+(TestStatistics left, TestStatistics right)
+            {
+                return new TestStatistics(
+                    left.SuccessfulCommandCount + right.SuccessfulCommandCount,
+                    left.FailedCommandCount + right.FailedCommandCount,
+                    left.UnknownCommandCount + right.UnknownCommandCount);
+            }
+
+            /// <summary>
+            /// An empty set of test statistics: the test statistics for a file that
+            /// does not execute any commands.
+            /// </summary>
+            public static readonly TestStatistics Empty = new TestStatistics(0, 0, 0);
+
+            /// <summary>
+            /// A set of test statistics that represent the successful execution of a single command.
+            /// </summary>
+            public static readonly TestStatistics SingleSuccess = new TestStatistics(1, 0, 0);
+
+            /// <summary>
+            /// A set of test statistics that represent a single failed command.
+            /// </summary>
+            public static readonly TestStatistics SingleFailure = new TestStatistics(0, 1, 0);
+
+            /// <summary>
+            /// A set of test statistics that represent a single unknown command.
+            /// </summary>
+            public static readonly TestStatistics SingleUnknown = new TestStatistics(0, 0, 1);
+        }
+
+        /// <summary>
         /// Runs a script, encoded as a sequence of expressions.
         /// </summary>
         /// <param name="expressions">The script, parsed as a sequence of expressions.</param>
-        public void Run(IEnumerable<SExpression> expressions)
+        public TestStatistics Run(IEnumerable<SExpression> expressions)
         {
+            var results = TestStatistics.Empty;
             foreach (var item in expressions)
             {
-                Run(item);
+                results += Run(item);
             }
+            return results;
         }
 
         /// <summary>
         /// Runs a script, encoded as a sequence of tokens.
         /// </summary>
         /// <param name="tokens">The script, parsed as a sequence of tokens.</param>
-        public void Run(IEnumerable<Lexer.Token> tokens)
+        public TestStatistics Run(IEnumerable<Lexer.Token> tokens)
         {
-            Run(Parser.ParseAsSExpressions(tokens, Log));
+            return Run(Parser.ParseAsSExpressions(tokens, Log));
         }
 
         /// <summary>
@@ -70,16 +156,16 @@ namespace Wasm.Text
         /// </summary>
         /// <param name="script">The text of the script to run.</param>
         /// <param name="scriptName">The file name of the script to run.</param>
-        public void Run(string script, string scriptName = "<string>")
+        public TestStatistics Run(string script, string scriptName = "<string>")
         {
-            Run(Lexer.Tokenize(script, scriptName));
+            return Run(Lexer.Tokenize(script, scriptName));
         }
 
         /// <summary>
         /// Runs a single expression in the script.
         /// </summary>
         /// <param name="expression">The expression to run.</param>
-        public void Run(SExpression expression)
+        public TestStatistics Run(SExpression expression)
         {
             if (expression.IsCallTo("module"))
             {
@@ -98,6 +184,7 @@ namespace Wasm.Text
                 {
                     instance.Functions[(int)module.StartFunctionIndex.Value].Invoke(Array.Empty<object>());
                 }
+                return TestStatistics.Empty;
             }
             else if (expression.IsCallTo("register"))
             {
@@ -113,10 +200,12 @@ namespace Wasm.Text
                 {
                     importer.RegisterImporter(name, new ModuleExportsImporter(moduleInstancesByName[moduleId]));
                 }
+                return TestStatistics.Empty;
             }
             else if (expression.IsCallTo("invoke") || expression.IsCallTo("get"))
             {
                 RunAction(expression);
+                return TestStatistics.SingleSuccess;
             }
             else if (expression.IsCallTo("assert_return"))
             {
@@ -138,9 +227,10 @@ namespace Wasm.Text
                             string.Join(", ", expected),
                             ".",
                             Assembler.Highlight(expression)));
-                    return;
+                    return TestStatistics.SingleFailure;
                 }
 
+                bool failures = false;
                 for (int i = 0; i < expected.Length; i++)
                 {
                     if (!object.Equals(results[i], expected[i]))
@@ -157,6 +247,7 @@ namespace Wasm.Text
                                     expected[i].ToString(),
                                     ".",
                                     Assembler.Highlight(expression)));
+                            failures = true;
                         }
                         else
                         {
@@ -170,9 +261,11 @@ namespace Wasm.Text
                                     expected[i].ToString(),
                                     ".",
                                     Assembler.Highlight(expression)));
+                            failures = true;
                         }
                     }
                 }
+                return failures ? TestStatistics.SingleFailure : TestStatistics.SingleSuccess;
             }
             else if (expression.IsCallTo("assert_trap") || expression.IsCallTo("assert_exhaustion"))
             {
@@ -205,7 +298,11 @@ namespace Wasm.Text
                     exception = ex;
                 }
 
-                if (!caught)
+                if (caught)
+                {
+                    return TestStatistics.SingleSuccess;
+                }
+                else
                 {
                     if (exception == null)
                     {
@@ -215,7 +312,6 @@ namespace Wasm.Text
                                 "assertion failed",
                                 "action should have trapped, but didn't.",
                                 Assembler.Highlight(expression)));
-                        throw new Exception("Assertion failed");
                     }
                     else
                     {
@@ -226,8 +322,8 @@ namespace Wasm.Text
                                 "action trapped as expected, but with an unexpected exception. ",
                                 exception.ToString(),
                                 Assembler.Highlight(expression)));
-                        throw exception;
                     }
+                    return TestStatistics.SingleFailure;
                 }
             }
             else if (expression.IsCallTo("assert_return_canonical_nan"))
@@ -246,7 +342,7 @@ namespace Wasm.Text
                             string.Join(", ", results),
                             "); expected a single canonical NaN.",
                             Assembler.Highlight(expression)));
-                    return;
+                    return TestStatistics.SingleFailure;
                 }
                 else if (results[0] is double)
                 {
@@ -264,7 +360,11 @@ namespace Wasm.Text
                 {
                     isCanonicalNaN = false;
                 }
-                if (!isCanonicalNaN)
+                if (isCanonicalNaN)
+                {
+                    return TestStatistics.SingleSuccess;
+                }
+                else
                 {
                     Log.Log(
                         new LogEntry(
@@ -274,6 +374,7 @@ namespace Wasm.Text
                             results[0].ToString(),
                             "; expected a single canonical NaN.",
                             Assembler.Highlight(expression)));
+                    return TestStatistics.SingleFailure;
                 }
             }
             else if (expression.IsCallTo("assert_return_arithmetic_nan"))
@@ -292,7 +393,7 @@ namespace Wasm.Text
                             string.Join(", ", results),
                             "); expected a single NaN.",
                             Assembler.Highlight(expression)));
-                    return;
+                    return TestStatistics.SingleFailure;
                 }
                 else if (results[0] is double)
                 {
@@ -306,7 +407,11 @@ namespace Wasm.Text
                 {
                     isNaN = false;
                 }
-                if (!isNaN)
+                if (isNaN)
+                {
+                    return TestStatistics.SingleSuccess;
+                }
+                else
                 {
                     Log.Log(
                         new LogEntry(
@@ -316,6 +421,7 @@ namespace Wasm.Text
                             results[0].ToString(),
                             "; expected a single NaN.",
                             Assembler.Highlight(expression)));
+                    return TestStatistics.SingleFailure;
                 }
             }
             else
@@ -329,6 +435,7 @@ namespace Wasm.Text
                             expression.Head.Span.Text,
                             " was not recognized as a known script command."),
                         Assembler.Highlight(expression)));
+                return TestStatistics.SingleFailure;
             }
         }
 
